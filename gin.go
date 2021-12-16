b97cdda9ad1aade9a1ffc6514da14c764f782e61
sunguangzong <sunguangzong@megvii.com>
2021-12-16 10:37:03 +0800

    FEAT：测试Gin 框架获取url 中的param

---
+++ gin.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"name": "lihua",
+		})
+	})
+	r.GET("/:id_type/:id_number", func(c *gin.Context) {
+		idType := c.Param("id_type")
+		idNumber := c.GetInt("id_number") // 不能获取url 中的id_number
+		c.JSON(200, gin.H{
+			idType: idNumber,
+		})
+	})
+	r.Run()
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/apache/rocketmq-client-go/v2 v2.1.0
+	github.com/gin-gonic/gin v1.7.7 // indirect
 	github.com/pkg/errors v0.8.1
 	github.com/sipt/GoJsoner v0.0.0-20170413020122-3e1341522aa6 // indirect
 	github.com/sirupsen/logrus v1.4.1
